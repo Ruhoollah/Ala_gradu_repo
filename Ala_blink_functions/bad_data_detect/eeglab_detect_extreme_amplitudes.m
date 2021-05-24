5a16a0c895d8f6ab1ffa0aeeebb577dc2469ae6b
Ala <64066252+Ruhoollah@users.noreply.github.com>
2021-05-24 12:12:22 +0300

    Add files via upload

---
+++ Ala_blink_functions/bad_data_detect/eeglab_detect_extreme_amplitudes.m
@@ -0,0 +1,246 @@
+function [EEG, Rej] = eeglab_detect_extreme_amplitudes(EEG, varargin)
+%EEGLAB_DETECT_EXTREME_AMPLITUDES - Detection of extreme amplitudes using simple thresholding 
+%
+% Description:
+%   Performs distribution/histogram analysis for sample amplitudes for all 
+%   channels specified using varargin 'rejectionChannels'. The default is
+%   all channels.
+%   Uses amplitude histogram quantiles and fixed thresholding to assign 
+%   samples as bad. If enough channels are bad for a given sample then the
+%   sample is marked bad for all channels i.e. an event is created into
+%   EEG.event.  
+%
+% Algorithm:
+%   1. Compute quantiles separately for each channel
+%   2. Select which rejection threshold to use: use either default limits
+%      or quantile limits, whichever is bigger
+%   3. Assign samples as bad for all channels: if more than coOccurrencePrc
+%      of the channels have marked the sample the sample is marked bad
+%   4. Create events and return results
+%
+% Syntax:
+%   [EEG, Rej] = eeglab_detect_extreme_amplitudes(EEG, varargin);
+%
+% Inputs:
+%   EEG     struct, EEGLAB EEG dataset
+%
+%   varargin    Keyword-value pairs
+%   Keyword         Type, description, values
+%
+%
+% Outputs:
+%   EEG     struct, EEGLAB struct with bad segments inserted as events,
+%           whose type is defined by varargin 'eventIdStr'.
+%   Rej     struct, Details about the rejection. Only for channels listed
+%           by varargin 'rejectionChannels'.
+%       .th         [nchan, 2] numeric, Actually used lower and upper 
+%                   rejection threshold in muV
+%       .qntTh      [nchan, 2] numeric, Quantile based rejection threshold
+%                   in muV
+%       .defaultTh  [1,2] numeric, Default rejection threshold in muV
+%       .match      [nchan, nsamples] logical, Bad segment matches for all 
+%                   analyzed channels
+%       .allChannelsMatch  [1, nsamples] logical, Final "all channel" bad segment
+%                   matches
+%       .allChannelsPrc    [1,1] numeric in [0...1], Percentage of samples marked 
+%                   as belonging to a bad segment
+%       .allChannelsCount      [1,1] integer, Number of bad segment events
+%
+% Assumptions:
+%
+% References:
+%
+% Example:
+%
+% Notes:
+%
+% See also:
+%
+% Copyright 2015- Jussi Korpela FIOH, jussi.korpela@ttl.fi
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% MAYBEDO: Add the possibility to plot channel histograms and rejection
+% thresholds. Appropriate code exists but the plotting function behaves
+% badly when there are many channels.
+
+%% Parse input arguments and set varargin defaults
+p = inputParser;
+p.KeepUnmatched = true; %to avoid error due to non-matching varargin
+
+p.addRequired('EEG', @isstruct);
+p.addParameter('rejectionChannels', {EEG.chanlocs(:).labels}, @iscellstr);
+p.addParameter('filter', false, @islogical);
+p.addParameter('rejectionMaskWidth', 0.2, @isnumeric); %in seconds
+p.addParameter('tailPercentage', 0.001, @isnumeric); %percentage in [0...1]
+% Small probability on each channel but altogether might result in a high
+% proportion of data marked as bad. See also normalEEGAmpLimits.
+p.addParameter('normalEEGAmpLimits', [-75, 75], @isnumeric); %in muV
+p.addParameter('coOcurrencePrc', 0.25, @isnumeric); %percentage in [0...1]
+p.addParameter('eventIDStr', 'artefactAmpTh', @ischar);
+
+p.parse(EEG, varargin{:});
+Arg = p.Results;
+
+filter_func = 'widmann';
+
+%% Define rejection dataset
+rejChanMatch = ismember({EEG.chanlocs.labels}, Arg.rejectionChannels);
+
+
+%% Filter the data
+if Arg.filter
+    locutoff = 1;
+    hicutoff = 0;
+    filtorder = EEG.srate;
+    
+    switch filter_func
+        case 'eeglab'
+            revfilt = 0;
+            firtype = 'fir1';
+            epochframes = 0;
+            eegdata = EEG.data(rejChanMatch,:);
+            eegdata = eegfilt(eegdata, EEG.srate, locutoff, hicutoff,...
+                              epochframes, filtorder, revfilt, firtype);
+
+        case 'widmann'
+            %
+            TMP = pop_select(EEG, 'channel', rejChanMatch);
+            m = pop_firwsord('hamming', filtorder, [locutoff hicutoff]);
+            b = firws(m, [locutoff hicutoff] / (filtorder / 2), 'high'...
+                , windows('hamming', m + 1));
+            TMP = firfilt(TMP, b);
+            eegdata = TMP.data;
+    end
+else
+    eegdata = EEG.data(rejChanMatch,:); %unfiltered
+end
+
+
+%% Remove data baseline
+eegdata = eegdata - repmat(mean(eegdata, 2), 1, size(eegdata, 2));
+
+
+%% Plotting related setup
+%{
+nSamplesPerBin = 1000;
+channelOrder = {EEG.chanlocs(:).labels};
+plotGrid = [ceil(size(EEG.data,1)/2),2];
+showPlot = false;
+savePlot = false;
+savePath = cd();
+nSamplesPerBin = 50;
+[N_chan, N_samples] = size(eegdata);
+chan_names = {EEG.chanlocs(:).labels};
+N_hist_bins = round(N_samples/nSamplesPerBin);
+ydata = NaN(N_chan, 1, N_hist_bins);
+xdata = NaN(N_chan, N_hist_bins);
+plotorder = NaN(1,N_chan);
+
+%   ydata   dim1 = r = n*m = subplots
+%           dim2 = k = traces in subplot (i)
+%           dim3 = p = samples of trace(k_i) in subplot (i)
+%}
+
+
+%% Analyze data amplitudes for each channel
+rej_match = true(size(eegdata));
+qnt_th = NaN(size(rej_match,1),2);
+rej_th = NaN(size(rej_match,1),2);
+
+fprintf('Analyzing channels: %s...\n', strjoin(Arg.rejectionChannels, ', '))
+for i = 1:size(rej_match,1) %over channels
+    % Compute quantiles
+    qnt_th(i,:) = [ quantile(eegdata(i,:), (Arg.tailPercentage/2)),...
+                    quantile(eegdata(i,:), 1-(Arg.tailPercentage/2))];
+                    %[low, high]
+    
+    % Select default or quantile based limit (more extreme selected)
+    i_rej_low = min(Arg.normalEEGAmpLimits(1), qnt_th(i,1));
+    i_rej_high = max(Arg.normalEEGAmpLimits(2), qnt_th(i,2));
+    
+    rej_match(i,:) = ((eegdata(i,:) <= i_rej_low) | ...
+                      (eegdata(i,:) >= i_rej_high));
+   
+    rej_th(i,:) = [i_rej_low i_rej_high];
+
+    %{
+    % Plotting related
+    %hist(eegdata(i,:), N_hist_bins);
+    N_hist_bins = 100;
+    rej_prc = sum(rej_match(i,:))/EEG.pnts*100;
+    [i_n, i_binc] = hist(eegdata(i,:), N_hist_bins);
+    ydata(i,1,:) = i_n;
+    xdata(i,:) = i_binc;
+    
+    vert_line_pos(i) = {rej_th(i,:)}; 
+    legendstrs{i} = {[Arg.rejectionChannels{i} ': rejected ' num2str(rej_prc,'%2.3f') ' %']};
+    plotorder(i) = find(ismember(channelOrder, Arg.rejectionChannels{i}));
+    ylabels(i) = {'Hist bin count'};
+    xlabels = {'Amplitude muV'};
+    %}
+    
+    clear('i_*');
+end
+
+
+%% Refine segments found
+rej_match = double(rej_match); % convert to numeric
+
+% Make rejection a bit wider around artefacts
+rejmask = ones(1, round(EEG.srate*Arg.rejectionMaskWidth));
+for i=1:size(rej_match,1)
+    rej_match(i,:) = conv(rej_match(i,:), rejmask, 'same');
+    rej_match(i,:) = rej_match(i,:) > 0; %conversion to logical
+end
+
+
+%% Combine rejection match from all rejection channels
+% Needed as channels cannot have separate boundary events in EEGLAB
+chCountTh = max(1, floor(Arg.coOcurrencePrc * size(rej_match, 1)));
+rej_counts = sum(rej_match, 1);
+samprej_match = rej_counts >= chCountTh; %[1,EEG.pnts] logical
+% Expand overlaps
+samprej_match = conv(single(samprej_match), rejmask, 'same'); %make wider
+samprej_match = samprej_match > 0; %conversion to logical
+
+
+%% Add as events
+if sum(samprej_match)~=0
+    % found something to reject
+    rej_inds = find_contiguous_range(samprej_match, 1);
+    durat = rej_inds(:,2)-rej_inds(:,1);
+
+    % create th rejection events
+    th_event = eeglab_create_event(rej_inds(:,1),Arg.eventIDStr,...
+                                    'duration', num2cell(durat));
+    %th_event2 = eeglab_create_event(rej_inds(:,2),'artefact_amplitudeth_end');
+
+    EEGtmp = EEG;
+    EEGtmp = rmfield(EEGtmp, 'data');
+    EEGtmp.event = th_event;
+
+    if ~isempty(EEG.event)
+        EEG.event = eeglab_merge_event_tables(EEG.event, EEGtmp.event, 'ignor');
+    else
+        EEG.event = eeglab_merge_event_tables(EEGtmp.event, 'ignore');
+    end
+
+    clear('EEGtmp*');
+else
+    rej_inds = [];
+    disp('No artefactual segments found.')
+end  
+
+
+%% Set output
+% Channel specific 
+Rej.th = rej_th;
+Rej.defaultTh = Arg.normalEEGAmpLimits;
+Rej.qntTh = qnt_th;
+Rej.match = rej_match;
+
+% Concerning all channels
+Rej.allChannelsMatch = samprej_match;
+Rej.allChannelsPrc = sum(samprej_match)/EEG.pnts;
+Rej.allChannelsCount = size(rej_inds, 1);
+
+end %eeglab_detect_extreme_amplitudes
---
+++ Ala_blink_functions/bad_data_detect/vari_bad_chans.m
@@ -0,0 +1,102 @@
+function result = vari_bad_chans(EEG, EEGchan, bounds, varargin)
+%VARI_BAD_CHANS detects bad channels by variance
+% 
+% Description:
+%   vari_bad_chans takes an input EEG data struct and calculates the 
+%   base e logarithmic variance relative to the median channel average.
+%   Channels are then compared with the bounds given to recognize 
+%   "dead" (normalized variance below the lower limit) and "loose" 
+%   (above higher limit) channels.
+%   Found channels are marked as dead or loose the EEG.chanlocs struct
+%
+% Syntax:
+%   result = vari_bad_chans(EEG, EEGchan, bounds)
+%
+% Inputs:
+%   'EEG'       struct, Input EEG data
+%   'EEGchan'   vector, indices of channels to check for badness
+%   'bounds'    [1,1] or [1;2] or [1,2] numeric, Thresholds for the
+%               log relative channel variance.
+%               [1,2] -> lower and higher log-rel-variance bounds
+%               [1,1] or [1;2] -> distance in MADs from median, +- or as given
+% Varargin:
+%   take_worst_n    scalar, if >0, return those n channels with highest
+%                   max abs variance, even if no channels exceed threshold.
+%                   To ensure no channels are marked bad, enter bounds = NaN,
+%                   and the whole set of 1-n highest abs variance channels
+%                   will be marked bad. Otherwise, n becomes only a limit on
+%                   number of bad channels
+%                   default=0
+% 
+% Outputs:
+%   'result'    struct, dead = logical []
+%
+%
+% Copyright(c) 2015 FIOH:
+% Benjamin Cowley, Jari Torniainen (Jari.Torniainen@ttl.fi)
+%
+% This code is released under the MIT License
+% http://opensource.org/licenses/mit-license.php
+% Please see the file LICENSE for details.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Parse input arguments and set varargin defaults
+p = inputParser;
+p.addRequired('EEG', @isstruct);
+p.addRequired('EEGchan', @isnumeric);
+p.addRequired('bounds', @ismatrix);
+p.addParameter('take_worst_n', 0, @isscalar);
+p.addParameter('plot', true, @islogical);
+p.parse(EEG, EEGchan, bounds, varargin{:});
+Arg = p.Results;
+
+
+%% Calculate normalized channel variance
+eegchs = numel(find(EEGchan)); % find() in case some fool uses logical indexing
+chanVar = repmat(NaN, 1, eegchs); %#ok<*RPMTN>
+chanVarNorm = repmat(NaN, 1, eegchs);
+
+chanVar = var(EEG.data(EEGchan, :), 0, 2);
+chanVarNorm = log(chanVar / median(chanVar));
+
+
+%% Check for loose and dead channels
+if isscalar(bounds) || iscolumn(bounds)
+    % use the MAD approach
+    th = [NaN NaN];
+    nmad = bounds;
+else
+    % use fixed thresholds
+    th = bounds;
+    nmad = NaN;
+end
+
+Res = thresholdNplot(chanVarNorm, th, nmad, Arg.plot);
+
+%if user wants to kill worst n channels, or set n as limit of bad channels
+if Arg.take_worst_n > 0
+    [~, ix] = sort(abs(chanVarNorm), 'descend');
+    if all(Res.isInRange)
+        Res.isInRange(ix(1:Arg.take_worst_n)) = false;
+        tmp = chanVarNorm > nansumedian(chanVarNorm);
+        Res.isAbove(ix(1:Arg.take_worst_n)) = tmp(ix(1:Arg.take_worst_n));
+        Res.isBelow(ix(1:Arg.take_worst_n)) = ~tmp(ix(1:Arg.take_worst_n));
+    else
+        % use n as limit by 'cleaning' all channels less variant than channel.n
+        Res.isInRange(ix(Arg.take_worst_n + 1:end)) = true;
+        Res.isAbove(ix(Arg.take_worst_n + 1:end)) = false;
+        Res.isBelow(ix(Arg.take_worst_n + 1:end)) = false;
+    end
+end
+
+
+%% return logical checks and normalized variance
+% backward compatibility
+result.dead = Res.isBelow;
+result.loose = Res.isAbove;
+result.variance = chanVarNorm;
+
+result.th = Res; %the standard output for any thresholding function
+result.th.figtitle = 'log relative channel variance';
+
+end % vari_bad_chans()
