ed8c7905a38cadd4d646e3437148d0807cc39034
Ala <64066252+Ruhoollah@users.noreply.github.com>
2021-06-04 13:38:10 +0300

    Update Ala_blink.m

--- Ala_blink_pipeline/Ala_blink.m
+++ Ala_blink_pipeline/Ala_blink.m
@@ -1,25 +1,26 @@
+
 %%  MAIN parameters
 % set the input directory where your data is stored
 data_dir = 'C:\Users\Ala\Rdata';
+% result_dir ='C:\Users\Ala\Alaslab';
 % specify the file type of your data
 data_type = '*.set';
 % use sbj_filt to select all (or a subset) of available recordings
-sbj_filt = 1; %setdiff(1:12, [3 7]);
+sbj_filt = 1 ; %setdiff(can1:12, [3 7]);
 % use ctapID to uniquely name the base folder of the output directory tree
-ctapID = 'Ala_blink_centered';
+ctapID = 'Ala_blink_result';
 % use keyword 'all' to select all stepSets, or use some index
-set_select = 'all';
+set_select = 'All';
 % set the electrode for which to calculate and plot ERPs after preprocessing
 erploc = 'A1';
 
 % Runtime options for CTAP:
-PREPRO = true;
 STOP_ON_ERROR = true;
 OVERWRITE_OLD_RESULTS = true;
 
 %% Create the CONFIGURATION struct
 % First, define step sets & their parameters: sbf_cfg() is written by the USER
-[Cfg, ctap_args] = sbf_cfg(data_dir, ctapID);
+[Cfg, ctap_args] = sbf_cfg('C:\Users\Ala', ctapID);
 
 % Select step sets to process
 Cfg.pipe.runSets = set_select;
@@ -31,15 +32,13 @@ Cfg = get_meas_cfg_MC(Cfg, data_dir, 'eeg_ext', data_type, 'sbj_filt', sbj_filt)
 Cfg = ctap_auto_config(Cfg, ctap_args);
 
 %% Run the pipe
-if PREPRO
-    tic; %#ok<*UNRCH>
-    CTAP_pipeline_looper(Cfg,...
-                        'debug', STOP_ON_ERROR,...
-                        'overwrite', OVERWRITE_OLD_RESULTS);
-    toc;
-end
+tic
+CTAP_pipeline_looper(Cfg,...
+                    'debug', STOP_ON_ERROR,...
+                    'overwrite', OVERWRITE_OLD_RESULTS);
+toc
 %cleanup the global workspace
-clear PREPRO STOP_ON_ERROR OVERWRITE_OLD_RESULTS ctap_args
+clear STOP_ON_ERROR OVERWRITE_OLD_RESULTS ctap_args
 
 %% Subfunctions
 % Pipe definition
@@ -55,7 +54,7 @@ Cfg.env.paths.projectRoot = project_root_folder;
 Cfg.env.paths.ctapRoot = fullfile(Cfg.env.paths.projectRoot, Cfg.id);
 % CTAP output goes into analysisRoot dir, here can be same as CTAP root
 Cfg.env.paths.analysisRoot = Cfg.env.paths.ctapRoot;
-
+%Cfg.eeg.chanlocs = fullfile('C:\','Users','Ala','EEGdata','Sub_2','chanlocs_sub3','sub-003_FaceRecog_electrodes.tsv');
 
 %% Define other important stuff
 Cfg.eeg.reference = {'average'};
@@ -74,9 +73,13 @@ clear('stepSet');
 
 i = 1; %stepSet 1
 stepSet(i).funH = { @CTAP_load_data,... 
-                    @CTAP_blink2event};
+                    @CTAP_blink2event,...
+                    @CTAP_reject_data};
                 
 stepSet(i).id = [num2str(i) '_load'];
+
+out.fir_filter = struct(...
+   'locutoff', 1);
 
 
 i = i+1;  %stepSet 2
@@ -88,21 +91,36 @@ stepSet(i).srcID = '';
 out.run_ica = struct(...
     'method', 'fastica',...
     'overwrite', true);
-out.run_ica.channels = {'EEG' 'EOG'};
+out.run_ica.channelTypes = [1 33 52:111 114:119 131 132];
+
+%i = i+1;  %stepSet 3
+%stepSet(i).funH = { @CTAP_detect_bad_comps,... %ADJUST for horizontal eye moves
+ %                   @CTAP_reject_data,...
+  %                  };
+%stepSet(i).id = [num2str(i) '_artifact_correction'];
+
+%ut.detect_bad_comps = struct(...
+%    'method', {'adjust' 'blink_template'},...
+%    'adjustarg', {'horiz' ''});
+
 
 i = i+1;  %stepSet 3
-stepSet(i).funH = { @CTAP_detect_bad_comps,...
-                    @CTAP_reject_data,...
-                    @CTAP_peek_data };
+
+stepSet(i).funH = { @CTAP_detect_bad_comps,... %detect blink related ICs
+                    @CTAP_filter_blink_ica,...@CTAP_detect_bad_comps,...
+                    @CTAP_peek_data};
+
 stepSet(i).id = [num2str(i) '_IC_CORRECTION'];
 
 out.detect_bad_comps = struct(...
-    'method', 'blink_template');
+    'method', 'recu_blink_tmpl');
+out.detect_bad_comps.test_pc = 30; 
+
 
 out.peek_data = struct(...
     'secs', [10 30],... %start few seconds after data starts
-    'peekStats', true,... %get statistics for each peek!
-    'overwrite', false,...
+    'peekStats', false,... %get statistics for each peek!
+    'overwrite', true,...
     'plotAllPeeks', false,...
     'savePeekData', true,...
     'savePeekICA', true);
@@ -113,3 +131,4 @@ Cfg.pipe.stepSets = stepSet; % return all step sets inside Cfg struct
 
 
 end
+
